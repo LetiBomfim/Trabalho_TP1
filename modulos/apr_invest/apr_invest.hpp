e189575d53a6d4e97d6eabd8b7854625754b660f
Pedro Souza Batista <pedrosouzab.p75@gmail.com>
2025-07-05 09:10:57 -0300

    docs(apr_invest.hpp): Criação da documentação externa

--- modulos/apr_invest/apr_invest.hpp
+++ modulos/apr_invest/apr_invest.hpp
@@ -4,6 +4,10 @@
 
 using namespace std;
 
+/**
+ * @brief Controlador da camada de apresentação para operações de investimento.
+ Permite criar, editar, excluir e listar carteiras e investimentos.
+ */
 class CtrlAprInvest: public InterApresInvest {
     private:
         static const int32_t LISTAR_CARTEIRA = 0;
@@ -27,10 +31,15 @@ class CtrlAprInvest: public InterApresInvest {
         void listar_invest();
 
     public:
-
+        /**
+        * @brief Construtor padrão. Inicializa o serviço de investimentos.
+        */
         CtrlAprInvest() {
             this->serv_invest = new ServInvest();
         }
-
+        /**
+        * @brief Inicia o fluxo de apresentação das operações de investimento para o usuário informado.
+        * @param cpf Identificador do usuário autenticado.
+        */
         void executar(const Cpf&);
 };
